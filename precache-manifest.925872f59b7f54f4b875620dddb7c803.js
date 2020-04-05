b2db71976d8b6e0755421ca636217b34e5ec2d0a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-05 17:16:11 -0500

    Updates

---
+++ precache-manifest.925872f59b7f54f4b875620dddb7c803.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a696023084e106a638eb35530345b329",
+    "url": "/react-hangman-v2/index.html"
+  },
+  {
+    "revision": "d648986f32e620e7299d",
+    "url": "/react-hangman-v2/static/css/main.bd34631b.chunk.css"
+  },
+  {
+    "revision": "4d5d2cbaf359af607fa2",
+    "url": "/react-hangman-v2/static/js/2.fa53ca8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hangman-v2/static/js/2.fa53ca8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d648986f32e620e7299d",
+    "url": "/react-hangman-v2/static/js/main.17c76a32.chunk.js"
+  },
+  {
+    "revision": "08a92a0a708541c40c17",
+    "url": "/react-hangman-v2/static/js/runtime-main.725f4ac3.js"
+  },
+  {
+    "revision": "f06df755b65f5f4d5c3b0a27a2d38f57",
+    "url": "/react-hangman-v2/static/media/0.f06df755.jpg"
+  },
+  {
+    "revision": "ca6c10d5826783b2e21485e1e92df1ce",
+    "url": "/react-hangman-v2/static/media/1.ca6c10d5.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/react-hangman-v2/static/media/10.c80394c2.jpg"
+  },
+  {
+    "revision": "1dc765683556a2c05162544cff514491",
+    "url": "/react-hangman-v2/static/media/2.1dc76568.jpg"
+  },
+  {
+    "revision": "837148ce5a3b2f485463adac61b033ec",
+    "url": "/react-hangman-v2/static/media/3.837148ce.jpg"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/react-hangman-v2/static/media/4.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/react-hangman-v2/static/media/5.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/react-hangman-v2/static/media/6.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/react-hangman-v2/static/media/7.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/react-hangman-v2/static/media/8.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/react-hangman-v2/static/media/9.1b557aa7.jpg"
+  }
+]);
